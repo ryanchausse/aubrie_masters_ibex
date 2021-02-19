0ec2b0db7d2e3e22601ca0b0076cafa0b94f07bd
Ryan Chausse <chausse@gmail.com>
2021-02-18 16:07:58 -0800

    Changed: some more functionality in uploading files to IBEX farm. Corrected syntax for JSON file

--- replace_excel_formula.py
+++ replace_excel_formula.py
@@ -1,4 +1,4 @@
-import pandas as pd, os, time, base64, pysftp, requests, glob, datetime, pytz
+import pandas as pd, os, time, base64, pysftp, requests, glob, datetime, pytz, re, pathlib
 from dateutil import tz
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,14 +12,11 @@ print("This script replaces the old Excel formula/column from the original exper
       from the original spreadsheet, process them into a JSON field that needs be appended to \
       example_data.js (or perhaps another, better named file), and to upload these data \
       to IBEX farm which shows the project at https://spellout.net/ibexexps/aubrieamstutz/SPD/experiment.html \
-      and whose repo is nonexistent (technique literally called braindead by the original author). \
+      and whose repo is nonexistent (a technique literally called braindead by the original author). \
       It may be possible to update via the web UI via Selenium.")
 
 # Assigning env variables for SFTP url, username, and password based on ENV variables
 load_dotenv(dotenv_path='.env')
-
-# Original, working excel formula for json column:
-# =CHAR(91)&CHAR(91)&""""&A2&""""&", "&B2&CHAR(93)&", ""AcceptabilityJudgment"", {s: {html: ""<div style=\""width: 50em;\""><!––  trial_type="&A2&"  item_number="&B2&"  pron="&E2&"  cond="&F2&"  cond_code="&D2&"  attested="&K2&"  ––><p style=\""text-align: center;\"" hidden>"&SUBSTITUTE(H2,CHAR(10),"<br \> ")&"</p><center><img style=\""text-align:center;\"" src=\""https://ryanchausse.com/aubrie_masters/images/conversation_pics/"&B2&"_"&C2&".png\"" alt=\""" & SUBSTITUTE(H2,CHAR(10),"<br \> ") & " " & I2 & " " & J2 & "\"" /></center></div>""}}],"
 
 # Get excel file from Box, download to ~/Downloads
 box_jwt_config_location = str(os.environ.get('BOX_JWT_CONFIG_LOCATION'))
@@ -27,8 +24,14 @@ box_file_shared_link = str(os.environ.get('BOX_FILE_SHARED_LINK'))
 box_local_dir = str(os.environ.get('BOX_LOCAL_DIR'))
 box_user_id = str(os.environ.get('BOX_USER_ID'))
 box_filename_without_version_or_extension = str(os.environ.get('BOX_FILENAME_WITHOUT_VERSION_OR_EXTENSION'))
+experiment_data_file_name = str(os.environ.get('EXPERIMENT_DATA_FILE_NAME'))
 expected_excel_local_path = box_local_dir + "/" + box_filename_without_version_or_extension
 expected_excel_local_path = expected_excel_local_path + '*.xlsx'
+expected_json_data_path = box_local_dir + "/" + experiment_data_file_name
+ibex_farm_username = str(os.environ.get('IBEX_FARM_USERNAME'))
+ibex_farm_password = str(os.environ.get('IBEX_FARM_PASSWORD'))
+ibex_farm_project_name = str(os.environ.get('IBEX_FARM_PROJECT_NAME'))
+
 excel_local_path = './'
 for file in glob.glob(expected_excel_local_path):
     excel_local_path = file
@@ -56,9 +59,9 @@ dt_local_modified = datetime.datetime.strptime(local_file_modified_time, "%a %b 
 modified_recently = dt_box_modified > dt_local_modified
 
 # Write file content to file, else exit(0)
-if not modified_recently:
-    print('No change to file. Exiting...')
-    exit(0)
+# if not modified_recently:
+#     print('No change to file. Exiting...')
+#     exit(0)
 
 with open(excel_local_path, 'wb') as open_file:
     client.with_shared_link(box_file_shared_link, box_shared_link_password).file(box_file_id).download_to(open_file)
@@ -73,3 +76,55 @@ print(df)
 # local_dir = str(os.environ.get('LOCAL_DIR'))
 # ssh_login_name = os.environ.get('SSH_LOGIN_NAME')
 # ssh_password = os.environ.get('SSH_PASSWORD')
+
+# Original, working excel formula for json column:
+# =CHAR(91)&CHAR(91)&""""&A2&""""&", "&B2&CHAR(93)&", ""AcceptabilityJudgment"", {s: {html: ""<div style=\""width: 50em;\""><!––  trial_type="&A2&"  item_number="&B2&"  pron="&E2&"  cond="&F2&"  cond_code="&D2&"  attested="&K2&"  ––><p style=\""text-align: center;\"" hidden>"&SUBSTITUTE(H2,CHAR(10),"<br \> ")&"</p><center><img style=\""text-align:center;\"" src=\""https://ryanchausse.com/aubrie_masters/images/conversation_pics/"&B2&"_"&C2&".png\"" alt=\""" & SUBSTITUTE(H2,CHAR(10),"<br \> ") & " " & I2 & " " & J2 & "\"" /></center></div>""}}],"
+
+# Parse df, format as json value that needs to go into example_data.js
+# e.g. [["test", 6], "AcceptabilityJudgment", {s: {html: "<div style=\"width: 50em;\"><!––  trial_type=test  item_number=6  pron=We  cond=b  cond_code=Rt.Null  attested=Y  ––><p style=\"text-align: center;\" hidden>How was the rest of your Saturday?</p><center><img style=\"text-align:center;\" src=\"https://ryanchausse.com/aubrie_masters/images/conversation_pics/6_7.png\" alt=\"How was the rest of your Saturday? Had a nice dinner and then we went to see some live music after we saw the movie with you. \" /></center></div>"}}],
+# which translates to:
+# [["<<trial.type>>", <<Item.n>>], "AcceptabilityJudgment", {s: {html: "<div style=\"width: 50em;\"><!––  trial_type=<<trial.type>>  item_number=<<Item.n>>  pron=<<Pron>>  cond=<<cond>>  cond_code=<<cond.code>>  attested=<<attested (Y/N)>>  ––><p style=\"text-align: center;\" hidden><<Intro>></p><center><img style=\"text-align:center;\" src=\"https://ryanchausse.com/aubrie_masters/images/conversation_pics/<<Item.n>>_<<list>>.png\" alt=\"<<Intro>> <<Response1>> <<Response2>>\" /></center></div>"}}],
+
+json_to_append = ''
+for index, row in df.iterrows():
+    row_to_json_string = "[[\"" + str(row['trial.type']) + "\", " + str(row['Item.n']) + "], \"AcceptabilityJudgment\", {s: {html: \"<div style=\\\"width: 50em;\\\"><!––  trial_type=" + str(row['trial.type']) + " item_number=" + str(row['Item.n']) + " pron=" + str(row['Pron']) + " cond=" + str(row['cond']) + " cond_code=" + str(row['cond.code']) + " attested=" + str(row['attested (Y/N)']) + " ––><p style=\\\"text-align: center;\\\" hidden>" + str(row['Intro']) + "</p><center><img style=\\\"text-align:center;\\\" src=\\\"https://ryanchausse.com/aubrie_masters/images/conversation_pics/" + str(row['Item.n']) + "_" + str(row['list']) + ".png\\\" alt=\\\"" + (str(row['Intro']) if row['Intro'] and str(row['Intro']) != 'nan' else '') + " " + (str(row['Response1']) if row['Response1'] and str(row['Response1']) != 'nan' else '') + " " + (str(row['Response2']) if row['Response2'] and str(row['Response2']) != 'nan' else '') + "\\\"/></center></div>""}}],"
+    json_to_append += row_to_json_string + "\n"
+    print('')
+print(json_to_append)
+
+# Now replace all experiment JSON data with our new json_to_append
+# then append "];" so it is properly formatted JSON at end of file, and write to file.
+json_to_append = "// Begin experiment data: \n" + json_to_append + "];"
+txt = pathlib.Path(expected_json_data_path).read_text()
+old_file_data = txt.split('// Begin')[0]
+new_file_data = old_file_data + json_to_append
+
+with open(expected_json_data_path, 'w') as json_data_file:
+    json_data_file.write(new_file_data)
+
+# Now, log into IBEX farm with Selenium and replace example_data.js with our new version
+print('Replacing JSON data file in IBEX farm...')
+
+# Log in to IBEX Farm using Selenium
+driver = webdriver.Firefox()
+driver.get('https://spellout.net/ibexfarm/login')
+time.sleep(1)
+username_element = driver.find_element_by_name("username")
+username_element.send_keys(ibex_farm_username)
+time.sleep(1)
+password_element = driver.find_element_by_name("password")
+password_element.send_keys(ibex_farm_password)
+time.sleep(1)
+log_in_button = driver.find_element_by_name("submit")
+log_in_button.click()
+time.sleep(2)
+experiment_link = driver.find_element_by_link_text(ibex_farm_project_name)
+experiment_link.click()
+time.sleep(2)
+upload_new_file_to_data_includes_dir_element = driver.find_element_by_xpath('/html/body/div[12]/input')
+upload_new_file_to_data_includes_dir_element.send_keys(expected_json_data_path)
+sleep(2)
+driver.close()
+driver.quit()
+
+print('Done')
